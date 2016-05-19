a03eb50610c7fd5f08ac10e36f2a5f4f3d35b35c
Joseph Cardwell <joseph.cardwell@joomsavvy.com>
2016-05-19 13:53:01 -0500

    Add User Authentication

---
+++ public/build/src/app/auth/logout/controller.js
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+
+angular.module( 'app.auth.logout', [])
+
+    /**
+     * And of course we define a controller for our route.
+     */
+    .controller( 'AuthLogoutCtrl', function AuthLogoutController( $scope,$filter, $stateParams) {
+
+    })
+    ;
+    
