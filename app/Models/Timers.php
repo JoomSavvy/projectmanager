e6fb80d526f876c46edaabab2ef00eac1ef22d73
Joseph Cardwell <joseph.cardwell@joomsavvy.com>
2016-07-08 11:40:04 -0500

    commit

---
+++ app/Models/Timers.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @SWG\Definition(
+ *      definition="Users",
+ *      required={},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="name",
+ *          description="name",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="email",
+ *          description="email",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="password",
+ *          description="password",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="remember_token",
+ *          description="remember_token",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="created_at",
+ *          description="created_at",
+ *          type="string",
+ *          format="date-time"
+ *      ),
+ *      @SWG\Property(
+ *          property="updated_at",
+ *          description="updated_at",
+ *          type="string",
+ *          format="date-time"
+ *      )
+ * )
+ */
+class Users extends Model
+{
+    //use SoftDeletes;
+
+    public $table = 'users';
+    
+
+    //protected $dates = ['deleted_at'];
+
+
+    public $fillable = [
+        "id",
+        "name",
+        "email",
+        "password",
+        "isAdmin",
+        "remember_token",
+        "created_at",
+        "updated_at"
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+    
+    public function projects(){
+        return $this->belongsToMany('App\Models\Projects','projects_users','user_id','project_id');
+    }
+
+    public function tasks(){
+        return $this->belongsToMany('App\Models\Tasks','users_tasks','user_id','task_id');
+    }
+
+}
---
+++ app/Repositories/LocalBaseRepository.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joseph
+ * Date: 6/30/2016
+ * Time: 11:26 AM
+ */
